18a03f14534e4ff0996fd3e55daf07964141dce2
ryabovd <ryabovd@outlook.com>
2024-08-16 22:36:40 +0700

    Add func build_url for build url for search

--- bankrupt.py
+++ bankrupt.py
@@ -1,8 +1,17 @@
 import requests
 
 
+def build_url(prsnbankruptsId, regionId='95'):
+    '''Build str url for parse'''
+    start_url = "https://bankrot.fedresurs.ru/backend/prsnbankrupts?searchString="
+    middle_url = "&isActiveLegalCase=true&regionId="
+    end_url = "&limit=15&offset=0"
+    return start_url + prsnbankruptsId + middle_url + regionId + end_url
+
 def main():
-    url = "https://bankrot.fedresurs.ru/backend/prsnbankrupts?searchString=%D0%91%D0%B0%D0%B1%D0%BA%D0%B8%D0%BD%D0%B0%20%D0%9B%D0%B0%D1%80%D0%B8%D1%81%D0%B0%20%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%BD%D0%B0&isActiveLegalCase=true&regionId=95&limit=15&offset=0"
+    prsnbankruptsId = input('Введите ФИО или ИНН или СНИЛС ')
+    url = build_url(prsnbankruptsId)
+    "https://bankrot.fedresurs.ru/backend/prsnbankrupts?searchString=Романов&isActiveLegalCase=true&regionId=95&limit=15&offset=0"
     headers = {
         "Accept": "application/json, text/plain, */*",
         "Accept-Language": "ru-RU,ru;q=0.9,en-RU;q=0.8,en;q=0.7,en-US;q=0.6",
@@ -23,6 +32,11 @@ def main():
     print(response.text)
     print(response.headers)
 
+def func(): 
+    '''Get str in russian and codinng in str in url'''
+    
+
+    
 
 if __name__ == "__main__":
     main()
